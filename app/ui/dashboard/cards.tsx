796af65c680915bb1426de80a739b46f9c7f25fc
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-03-12 01:50:56 -0400

    fetching data from the dashboard overview page

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -30,15 +30,7 @@ export default async function CardWrapper() {
   );
 }
 
-export function Card({
-  title,
-  value,
-  type,
-}: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+export function Card({title, value, type,}: {title: string; value: number | string;type: 'invoices' | 'customers' | 'pending' | 'collected';}) {
   const Icon = iconMap[type];
 
   return (
@@ -47,10 +39,7 @@ export function Card({
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
+      <p className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}>
         {value}
       </p>
     </div>
